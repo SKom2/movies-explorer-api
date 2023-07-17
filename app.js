7222867b37ced61706885e4c12f52eff0de19ec2
Aleksandr Komolkin <san4es.kom@gmail.com>
2023-07-17 22:45:28 +0300

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const router = require('./src/routes');
+const router = require('./routes');
 
 const { CONNECT_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsbd', PORT = 3000 } = process.env;
 
